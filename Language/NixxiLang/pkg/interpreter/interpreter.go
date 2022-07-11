5244126800bf30ed72401479407a784c9c53a63d
nilphumiphat212 <nilphumiphat@outlook.co.th>
2022-07-11 18:36:17 +0700

    [NixxiLang][Nil] Add exit command

--- Language/NixxiLang/pkg/interpreter/interpreter.go
+++ Language/NixxiLang/pkg/interpreter/interpreter.go
@@ -4,34 +4,43 @@ import (
 	"NixxiLang/pkg/util"
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
-func NewInterpreter() {
+func NewInterpreter() error {
 	var args []string = os.Args
 	if len(args) > 1 {
 		tokens, err := LoadToLexerFromFile(args[1])
 		if err != nil {
-			log.Fatalln(err)
+			return err
 		}
 		util.WriteJsonFile("language_token.json", tokens)
 	} else {
-		interactiveInterpreter()
+		err := interactiveInterpreter()
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func interactiveInterpreter() {
+func interactiveInterpreter() error {
 	var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
 	for {
 		var text string
 		fmt.Print("Nixxi : ")
 		scanner.Scan()
 		text = scanner.Text()
-		token, err := convertTextLineToTokan(text)
-		if err != nil {
-			log.Fatalln(err)
+		if len(text) > 0 {
+			if text == "exit" {
+				break
+			}
+			token, err := convertTextLineToTokan(text)
+			if err != nil {
+				return err
+			}
+			fmt.Println(token)
 		}
-		fmt.Println(token)
 	}
+	return nil
 }
