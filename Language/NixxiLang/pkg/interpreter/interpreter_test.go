5a7fb23d4edad931dba27a5eb3e466aa0d0d75a8
nilphumiphat212 <nilphumiphat@outlook.co.th>
2022-07-11 19:14:11 +0700

    [NixxiLang][Nil] Add interpreter test

--- Language/NixxiLang/pkg/interpreter/interpreter_test.go
+++ Language/NixxiLang/pkg/interpreter/interpreter_test.go
@@ -1,1 +1,42 @@
 package interpreter
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewInterpreter(t *testing.T) {
+	os.Args = []string{"main.go", "../../test.nixxi"}
+	err := NewInterpreter()
+	if err != nil {
+		t.Error("Can not get command line arguments")
+	}
+	os.Remove("language_token.json")
+
+	// TODO: can not mock Stdin
+
+	// os.Args = []string{}
+	// tempFile, err := ioutil.TempFile("test_dir", "test")
+	// if err != nil {
+	// 	t.Error("Can not mock stdin")
+	// }
+
+	// input := []byte("exit\n")
+
+	// write, err := tempFile.Write(input)
+	// if err != nil {
+	// 	t.Error("Can not mock stdin (Can not write temp file status : " + strconv.Itoa(write))
+	// }
+
+	// seek, err := tempFile.Seek(0, 0)
+	// if err != nil {
+	// 	t.Error("Can not mock stdin (Can not write seek status : " + strconv.Itoa(int(seek)))
+	// }
+
+	// os.Stdin = tempFile
+
+	// err2 := NewInterpreter()
+	// if err2 != nil {
+	// 	t.Error("Can not open interactive interpreter when args empty")
+	// }
+}
