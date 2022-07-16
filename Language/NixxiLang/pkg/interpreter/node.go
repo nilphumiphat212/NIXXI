2830e5b6ae33e0403ce6565b2c0d95c30935be17
nilphumiphat212 <nilphumiphat@outlook.co.th>
2022-07-16 19:14:52 +0700

    [NixxiLang][Nil] Add node r1

--- Language/NixxiLang/pkg/interpreter/node.go
+++ Language/NixxiLang/pkg/interpreter/node.go
@@ -1,1 +1,60 @@
 package interpreter
+
+type Node interface {
+	getNodeName() string
+}
+
+type VarNode struct {
+	Line         int
+	VariableName []byte
+	Type         VarType
+	Value        []byte
+}
+
+func (varNode VarNode) getNodeName() string {
+	return "VAR_NODE"
+}
+
+type MutNode struct {
+	Line         int
+	VariableName []byte
+}
+
+func (mutNode MutNode) getNodeName() string {
+	return "MUT_NODE"
+}
+
+type IfNode struct {
+	Line       int
+	LeftValue  any
+	RightValue any
+	Operator   OperatorValue
+	Children   []Node
+}
+
+func (ifNode IfNode) getNodeName() string {
+	return "IF_NODE"
+}
+
+type ElseNode struct {
+	Line     int
+	Children []Node
+}
+
+func (ElseNode ElseNode) getNodeName() string {
+	return "ELSE_NODE"
+}
+
+type ElseIfNode struct{}
+
+type LoopNode struct{}
+
+type ReturnNode struct {
+	Line  int
+	From  []byte
+	Value any
+}
+
+func (returnNode ReturnNode) getNodeName() string {
+	return "RETURN_NODE"
+}
--- Language/NixxiLang/pkg/interpreter/parser.go
+++ Language/NixxiLang/pkg/interpreter/parser.go
@@ -1,9 +1,16 @@
 package interpreter
 
-type ExpressionNode struct {
-	Type TokenType
-}
-
-func Parser() {
-
+func ParserMultiLine(tokens *[][]Token) []Node {
+	var nodes []Node
+	for line, tokenInLine := range *tokens {
+		for _, token := range tokenInLine {
+			nodes = append(nodes, VarNode{
+				Line:         line,
+				VariableName: []byte(token.Type),
+				Type:         VARTYPE_NULL,
+				Value:        []byte(token.Value),
+			})
+		}
+	}
+	return nodes
 }
